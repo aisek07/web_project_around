76fb5a7c49923a6f8fb2766aeb62c870be2118b9
aisek <kesialeticia58@gmail.com>
2024-12-18 21:23:25 -0300

    Co-authored-by: Claudio Cassimiro <claudiocassimiro@users.noreply.github.com>

---
+++ scripts/index.js
@@ -0,0 +1,16 @@
+console.log("mensagem");
+ // Função para mostrar o popup
+ function showPopup() {
+    document.getElementById('popup-overlay').style.display = 'block';
+}
+
+// Função para esconder o popup
+function hidePopup() {
+    document.getElementById('popup-overlay').style.display = 'none';
+}
+
+// Função para salvar os dados
+function saveData() {
+    const input1Value = document.getElementById('input1').value;
+    const input2Value = document.getElementById('input2').value;
+}
