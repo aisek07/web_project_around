c0c3f02b4787e188899fe3670bb5ad83d8fe231e
aisek <kesialeticia58@gmail.com>
2025-01-14 13:57:18 -0300

    atualizando quase finalizado

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,10 @@
 console.log("mensagem");
 
 // Função para abrir a janela pop-up
-function openPopup() {
-  document.getElementById("popup").style.display = "block";
-  document.getElementById("overlay").style.display = "block";
+function addbutton() {
+  console.log("ativar botão editar");
+  document.getElementById("popup").style.display = "flex";
+  // document.getElementById("overlay").style.display = "block";//
 }
 
 // Função para fechar a janela pop-up
@@ -18,14 +19,18 @@ function openPopup() {
 }
 
 // Função para fechar a janela pop-up
-function closePopup() {
+function hidePopup() {
   document.getElementById("popup").style.display = "none"; // Oculta a janela pop-up
-  document.getElementById("overlay").style.display = "none"; // Oculta o fundo opaco
+  //document.getElementById("overlay").style.display = "none"; // Oculta o fundo opaco
 }
 
 // Função para salvar o nome (apenas um exemplo, pode ser adaptado)
-function saveName() {
+function saveData() {
   var nome = document.getElementById("nomeInput").value;
   alert("Nome salvo: " + nome);
   closePopup(); // Fecha a janela após salvar o nome
 }
+
+document.getElementById("card__like").addEventListener("click", function () {
+  this.classList.toggle("active"); // Alterna a classe 'active' no botão
+});
