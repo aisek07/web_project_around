1eac7a9e55be2a2c0895c6e97422a90b463c7935
aisek07 <kesialeticia58@gmail.com>
2025-08-28 19:00:33 -0400

    atualizacao

--- scripts/index.js
+++ scripts/index.js
@@ -45,6 +45,83 @@ const formCreateCard = document.querySelector("#cardForm");
 const cardPopupSubmit = document.querySelector("#image_submit");
 console.log(formCreateCard)
 
+
+// mensagem de erro//
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add("form__input_type_error");
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add("form__input-error_active");
+};
+
+const hideInputError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove("form__input_type_error");
+  errorElement.classList.remove("form__input-error_active");
+  errorElement.textContent = "";
+};
+
+const checkInputValidity = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+};
+
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  });
+};
+
+const toggleButtonState = (inputList, buttonElement) => {
+  console.log(hasInvalidInput(inputList));
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add("button_inactive");
+  } else {
+    buttonElement.classList.remove("button_inactive");
+  }
+};
+
+const setEventListeners = (formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll(".form__input"));
+  const buttonElement = formElement.querySelector(".form__submit");
+
+  // Aqui, para verificar o estado do botão no início:
+  toggleButtonState(inputList, buttonElement);
+
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener("input", function () {
+      checkInputValidity(formElement, inputElement);
+      // E aqui, para verificar sempre que qualquer entrada de campo for alterada:
+      toggleButtonState(inputList, buttonElement);
+    });
+  });
+};
+
+const enableValidation = () => {
+  const formList = Array.from(document.querySelectorAll(".form"));
+  formList.forEach((formElement) => {
+    formElement.addEventListener("submit", function (evt) {
+      evt.preventDefault();
+    });
+
+    const fieldsetList = Array.from(formElement.querySelectorAll(".form__set"));
+
+    fieldsetList.forEach((fieldset) => {
+      setEventListeners(fieldset);
+    });
+  });
+};
+
+enableValidation();
+
+
+// validacao input 
+
+
+
 openBtn.addEventListener('click', () => {
   editPopup.classList.add('popup_opened');
 })
@@ -194,24 +271,7 @@ function saveImage(e) {
 formCreateCard.addEventListener('submit', saveImage);
 
 
-
-// function saveImage(event){
-//   event.preventDefault()
-//   console.log("chamado")
-//   const imageName = inputImageName.value
-//   const imageLink = inputLink.value
-// createCard({
-//   name:imageName,
-//   link:imageLink,
-
-// })
-
- 
-//  inputImageName.value = ""
-//   inputJob.value = ""
-
-
-// }
-// console.log(cardPopupSubmit)
-// cardPopupSubmit.addEventListener("click",saveImage);
-
+// validar input//
+formInput.addEventListener("input", function (evt) {
+console.log(evt.target.validity);
+});
